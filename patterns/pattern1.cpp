21587f82588cc03a9db6d75e5701ede92093fc9e
Sid <thesenjubusiness@gmail.com>
2026-07-02 10:15:20 +0530

    message

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){
+
+    //method 1
+    for(int i=0; i < 5; i++){
+        for(int j=0; j < 5; j++){
+            cout << "* "; 
+        }
+        cout << "\n"; 
+    }
+
+    //method 2 
+    /*    for(int i=0; i < 5; i++){
+            cout << "* * * * * \n"; 
+        }
+    */
+
+    return 0; 
+}
